9ae82a05dd530876d2d90feefae8885880942d03
Holger Banko <h.banko@gmail.com>
2019-12-14 16:30:19 +1100

    Initial Commit

---
+++ barApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  barApp
+//
+//  Created by Holger Banko on 14/12/19.
+//  Copyright © 2019 Holger Banko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ barApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  barApp
+//
+//  Created by Holger Banko on 14/12/19.
+//  Copyright © 2019 Holger Banko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
